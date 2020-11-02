26afe56e82ab3ba334288d026670c8f699c8de2e
sanposhiho <handbomusic@gmail.com>
2020-11-03 00:33:57 +0900

    fix: detail bug

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :tsundoku_buster, TsundokuBuster.Repo,
-  username: "root",
-  password: "",
+  username: "app",
+  password: "Password",
   database: "tsundoku_buster_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TsundokuBuster.MixProject do
 
   def project do
     [
-      app: :TsundokuBuster,
+      app: :tsundoku_buster,
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
